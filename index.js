91f9941ace7ce4bc3224cdb570408071dbc23090
hoyeon05 <137599327+hoyeon05@users.noreply.github.com>
2025-06-17 13:39:47 +0900

    Add files via upload

--- index.js
+++ index.js
@@ -2,14 +2,15 @@ const express = require('express');
 const cors = require('cors');
 
 const app = express();
-app.use(cors());
+app.use(cors());  // ✅ 프론트에서 fetch 요청 허용
 
-const port = process.env.PORT || 5000;  // ✅ 이게 핵심!!
+// ✅ 반드시 이 포트로 받아야 Render에서 작동함
+const port = process.env.PORT || 5000;
 
 app.get('/bus-info', (req, res) => {
   res.json([
-    { name: "상록수역", lat: 37.3025, lng: 126.8667 },
-    { name: "안산대학교", lat: 37.3100, lng: 126.8235 }
+    { name: "상록수역", lat: 37.303611793223766, lng: 126.86688233780662 },
+    { name: "안산대학교", "lat": 37.30953435504419, "lng": 126.87350757149399, }
   ]);
 });
 
